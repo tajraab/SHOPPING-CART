c45ba39b28e971ecae737df25396d5d5b42c07b5
tajraab <tajrabacevac@gmail.com>
2023-11-17 00:52:03 +0100

    card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,40 +1,48 @@
 // import React from 'react';
 import "./Card.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-
- function Card() {
-  
-  const [selectedColor, setSelectedColor] = useState('#456ABD'); // Podesite početnu boju prema vašem izboru
-  const [selectedSize, setSelectedSize] = useState('M');
+function Card() {
+  const [selectedColor, setSelectedColor] = useState("#456ABD"); // Podesite početnu boju prema vašem izboru
+  const [selectedSize, setSelectedSize] = useState("M");
 
   const colorOptions = [
-    { color: 'lightgrey', img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt-large2.png' },
-    { color: '#456ABD', img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt-large.png' },
-    { color: '#EAA523', img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt-large3.png' },
+    { color: "lightgrey", img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt-large2.png" },
+    { color: "#456ABD", img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt-large.png" },
+    {
+      color: "#EAA523",
+      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt-large3.png",
+    },
   ];
 
   const handleColorChange = (color, img) => {
     setSelectedColor(color);
     document.body.style.background = color;
-    document.querySelector('.wrapper .price').style.color = color;
-    document.querySelectorAll('.size-value span.color').forEach(span => (span.style.color = color));
-    document.querySelector('.size-value span.active').style.background = color;
-    document.querySelector('.image img').src = img;
-    document.querySelectorAll('.btns button').forEach(btn => {
+    document.querySelector(".wrapper .price").style.color = color;
+    document
+      .querySelectorAll(".size-value span.color")
+      .forEach((span) => (span.style.color = color));
+    document.querySelector(".size-value span.active").style.background = color;
+    document.querySelector(".image img").src = img;
+    document.querySelectorAll(".btns button").forEach((btn) => {
       btn.style.background = color;
       btn.style.borderColor = color;
     });
   };
 
-  const handleSizeChange = size => {
+  const handleSizeChange = (size) => {
     setSelectedSize(size);
   };
 
   return (
     <div className="container">
       <div className="image">
-        <img src={colorOptions.find(option => option.color === selectedColor)?.img} alt="" />
+        <img
+          src={
+            colorOptions.find((option) => option.color === selectedColor)?.img
+          }
+          alt=""
+        />
       </div>
       <div className="card-content">
         <div className="wrapper">
@@ -44,19 +52,34 @@ import React, { useState } from 'react';
           <div className="content size">
             <div className="name size-name">Size</div>
             <div className="size-value">
-              <span className={`color ${selectedSize === 'XS' ? 'active' : ''}`} onClick={() => handleSizeChange('XS')}>
+              <span
+                className={`color ${selectedSize === "XS" ? "active" : ""}`}
+                onClick={() => handleSizeChange("XS")}
+              >
                 XS
               </span>
-              <span className={`color ${selectedSize === 'S' ? 'active' : ''}`} onClick={() => handleSizeChange('S')}>
+              <span
+                className={`color ${selectedSize === "S" ? "active" : ""}`}
+                onClick={() => handleSizeChange("S")}
+              >
                 S
               </span>
-              <span className={`color ${selectedSize === 'M' ? 'active' : ''}`} onClick={() => handleSizeChange('M')}>
+              <span
+                className={`color ${selectedSize === "M" ? "active" : ""}`}
+                onClick={() => handleSizeChange("M")}
+              >
                 M
               </span>
-              <span className={`color ${selectedSize === 'L' ? 'active' : ''}`} onClick={() => handleSizeChange('L')}>
+              <span
+                className={`color ${selectedSize === "L" ? "active" : ""}`}
+                onClick={() => handleSizeChange("L")}
+              >
                 L
               </span>
-              <span className={`color ${selectedSize === 'XL' ? 'active' : ''}`} onClick={() => handleSizeChange('XL')}>
+              <span
+                className={`color ${selectedSize === "XL" ? "active" : ""}`}
+                onClick={() => handleSizeChange("XL")}
+              >
                 XL
               </span>
             </div>
@@ -64,10 +87,12 @@ import React, { useState } from 'react';
           <div className="content colour">
             <div className="name colour-name">Colour</div>
             <div className="colour-value">
-              {colorOptions.map(option => (
+              {colorOptions.map((option) => (
                 <span
                   key={option.color}
-                  className={`${option.color} ${selectedColor === option.color ? 'active' : ''}`}
+                  className={`${option.color} ${
+                    selectedColor === option.color ? "active" : ""
+                  }`}
                   data-color={option.color}
                   data-img={option.img}
                   onClick={() => handleColorChange(option.color, option.img)}
@@ -84,6 +109,5 @@ import React, { useState } from 'react';
     </div>
   );
 }
-
 
 export default Card;
