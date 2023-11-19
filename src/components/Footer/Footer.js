fc10db63c387444e42d440608ed303cb68c36afe
tajraab <tajrabacevac@gmail.com>
2023-11-19 21:30:59 +0100

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,2 +1,14 @@
 import React from "react";
 import "./Footer.css";
+function Footer() {
+    return (
+      <div className="footer">
+        <div className="footer-container">
+          <div className="footer-section">
+            <h3>O nama</h3>
+            <p>
+              Mi smo online prodavnica posvećena pružanju vrhunskih proizvoda i
+              iskustava kupovine koje će vas oduševiti.
+            </p>
+          </div>
+          <div className="footer-section">
