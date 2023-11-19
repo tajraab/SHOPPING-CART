5ca40be135dc1a38a330f71c4cc19c0e0fbf0836
tajraab <tajrabacevac@gmail.com>
2023-11-19 21:42:37 +0100

    footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 function Footer() {
     return (
-      <div className="footer">
+        <div className="footer">
         <div className="footer-container">
           <div className="footer-section">
             <h3>O nama</h3>
@@ -13,26 +13,26 @@ function Footer() {
           </div>
           <div className="footer-section">
             <h3>Linkovi</h3>
-          <ul>
-            <li>
-              <a href="#">Početna</a>
-            </li>
-            <li>
-              <a href="#">Proizvodi</a>
-            </li>
-            <li>
-              <a href="#">Kontakt</a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-section">
-          <h3>Kontakt informacije</h3>
-          <p>Email: info@vasshop.com</p>
-          <p>Telefon: 123-456-789</p>
+            <ul>
+              <li>
+                <a href="#">Početna</a>
+              </li>
+              <li>
+                <a href="#">Proizvodi</a>
+              </li>
+              <li>
+                <a href="#">Kontakt</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-section">
+            <h3>Kontakt informacije</h3>
+            <p>Email: info@vasshop.com</p>
+            <p>Telefon: 123-456-789</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-export default Footer;
+    );
+  }
+  
+  export default Footer;
