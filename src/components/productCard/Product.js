47592046bb6f401d135c6aedad532c60134f1ebb
tajraab <tajrabacevac@gmail.com>
2023-11-19 21:59:57 +0100

    .

--- src/components/productCard/Product.js
+++ src/components/productCard/Product.js
@@ -16,3 +16,15 @@ function Card({ product, addToCart }) {
     return (
       <div className="main-cont">
         <div className="first-cont">
+        <img src={product.imageURL} alt={product.title} />
+      </div>
+      <div className="second-cont">
+        <h4>{product.title}</h4>
+        <h5 id="green-underline">Remove</h5>
+      </div>
+      <div className="third-cont">
+        <h4>{Number(product.price)}$</h4>
+        <div className="quantity">
+          <p style={{ cursor: "pointer" }} onClick={decrease}>
+            -
+          </p>
