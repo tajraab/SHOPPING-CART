65653a4aef09f46346d4134fb3d52ea7851b4091
tajraab <tajrabacevac@gmail.com>
2023-11-19 22:02:28 +0100

    product

--- src/components/productCard/Product.js
+++ src/components/productCard/Product.js
@@ -14,38 +14,39 @@ function Card({ product, addToCart }) {
     };
   
     return (
-      <div className="main-cont">
-        <div className="first-cont">
-        <img src={product.imageURL} alt={product.title} />
-      </div>
-      <div className="second-cont">
-        <h4>{product.title}</h4>
-        <h5 id="green-underline">Remove</h5>
-      </div>
-      <div className="third-cont">
-        <h4>{Number(product.price)}$</h4>
-        <div className="quantity">
-          <p style={{ cursor: "pointer" }} onClick={decrease}>
-            -
-          </p>
-          <p>{quantity}</p>
-          <p style={{ cursor: "pointer" }} onClick={increase}>
-            +
-          </p>
-        </div>
-        <div style={{ width: "40px" }}>
-          {product.discount && <h4>{product.discount}%</h4>}
-        </div>
-        <div
-          className="quantity"
-          onClick={addToCart}
-          style={{ width: "100px", cursor: "pointer", fontWeight: "bolder" }}
-        >
-          <p>Add to cart</p>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default Card;
+        return (
+            <div className="main-cont">
+              <div className="first-cont">
+                <img src={product.imageURL} alt={product.title} />
+              </div>
+              <div className="second-cont">
+                <h4>{product.title}</h4>
+                <h5 id="green-underline">Remove</h5>
+              </div>
+              <div className="third-cont">
+                <h4>{Number(product.price)}$</h4>
+                <div className="quantity">
+                  <p style={{ cursor: "pointer" }} onClick={decrease}>
+                    -
+                  </p>
+                  <p>{quantity}</p>
+                  <p style={{ cursor: "pointer" }} onClick={increase}>
+                    +
+                  </p>
+                </div>
+                <div style={{ width: "40px" }}>
+                  {product.discount && <h4>{product.discount}%</h4>}
+                </div>
+                <div
+                  className="quantity"
+                  onClick={addToCart}
+                  style={{ width: "100px", cursor: "pointer", fontWeight: "bolder" }}
+                >
+                  <p>Add to cart</p>
+                </div>
+              </div>
+            </div>
+          );
+        }
+        
+        export default Card;
