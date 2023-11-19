4819f0cbbf0a53f05e4b32e5a49c2438a743c76e
tajraab <tajrabacevac@gmail.com>
2023-11-19 21:59:01 +0100

    .

--- src/components/productCard/Product.js
+++ src/components/productCard/Product.js
@@ -3,4 +3,16 @@ import "./Product.css";
 import { useState } from "react";
 function Card({ product, addToCart }) {
     const [quantity, setQuantity] = useState(0);
+    const max = 10;
+    const increase = () => {
+      if (quantity !== max) {
+        setQuantity(quantity + 1);
+      }
+    };
+    const decrease = () => {
+      if (quantity > 0) setQuantity(quantity - 1);
+    };
   
+    return (
+      <div className="main-cont">
+        <div className="first-cont">
