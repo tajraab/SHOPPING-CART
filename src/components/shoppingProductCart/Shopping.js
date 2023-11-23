a837906ac54a217bb21a3bd445af9fb7066a27d9
tajraab <tajrabacevac@gmail.com>
2023-11-23 23:56:12 +0100

    .

--- src/components/shoppingProductCart/Shopping.js
+++ src/components/shoppingProductCart/Shopping.js
@@ -2,3 +2,11 @@ import React from "react";
 import "./Shopping.css";
 import { useState } from "react";
 
+function Shopping({ product, removeFromCart }) {
+    const [quantity, setQuantity] = useState(0);
+  
+    const max = 10;
+    const increase = () => {
+      if (quantity !== max) {
+        setQuantity(quantity + 1);
+      }
